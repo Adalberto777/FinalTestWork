c490f8d71032aa092bbfd99425f61bc2fe3e5e06
Adalberto777 <albert.tliashev@gmail.com>
2022-09-29 23:28:37 +0500

    сырая программа

---
+++ task/Program.cs
@@ -0,0 +1,47 @@
+﻿// программа, которая из существуещего массива строк формирует массив строк, длинна которых меньше или равна трем
+string[] str = {"hello", "2", "world", ":-)", "1"};
+
+void PrintArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write(array[i] + ", ");
+  }
+  Console.WriteLine();
+}
+
+int CounterShortString(string[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {        
+        if (array[i].Length < 4)
+        {
+            result++;
+        }        
+    }
+    return result;
+}
+
+string[] GetShortArray(string[] array, int counter)
+{
+    string[] strResult = new string[counter];
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            strResult[j] = array[i];
+            j++;
+        }
+    }
+    return strResult;
+}
+
+int count = CounterShortString(str);
+
+
+
+PrintArray(str);
+PrintArray(GetShortArray(str, count));
